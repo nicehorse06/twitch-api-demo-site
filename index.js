a89ba48e451c23c7fded01212a778c37b5e342e0
jimmyma <jimmyma@croxlink.com>
2018-06-20 10:32:59 +0800

    finish get nine streamer data

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 let targetUrl =
   "https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends&client_id=80stfocyvne9dzzxyvz4j4x9yl75bd";
 let re = new XMLHttpRequest();
-let data
 re.open("GET", targetUrl);
 re.onload = () => {
-  data = JSON.parse(re.responseText);
-  render_page();
+  let data = JSON.parse(re.responseText);
+  render_page(data);
 };
 re.send();
 
-let render_page = () => {
-  document.querySelector('#block-1 .display_name').innerText = data.streams[0].channel.display_name;
-  document.querySelector('#block-1 .streamer-name').innerText = data.streams[0].channel.name
-  document.querySelector('#block-1 .channel-info-photo').src = data.streams[0].channel.logo
-  document.querySelector('#block-1 .channel-image').style.backgroundImage = `url('${data.streams[0].preview.medium}')`
-}
-
+let render_page = (data) => {
+  for (let i = 0; i < 9; i++) {
+    document.querySelector(`#block-${i + 1} .display_name`).innerText =
+      data.streams[i].channel.display_name;
+    document.querySelector(`#block-${i + 1} .streamer-name`).innerText =
+      data.streams[i].channel.name;
+    document.querySelector(`#block-${i + 1} .channel-info-photo`).src =
+      data.streams[i].channel.logo;
+    document.querySelector(
+      `#block-${i + 1} .channel-image`
+    ).style.backgroundImage = `url('${data.streams[i].preview.medium}')`;
+  }
+};
