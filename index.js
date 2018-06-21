845f5d943d53cd166f404e38edacdb55f5b13ef1
jimmyma <jimmyma@croxlink.com>
2018-06-21 10:30:43 +0800

    add channel num to 20

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+let client_id = '80stfocyvne9dzzxyvz4j4x9yl75bd'
 let targetUrl =
-  "https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends&client_id=80stfocyvne9dzzxyvz4j4x9yl75bd";
+  `https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends&client_id=${client_id}`;
 let re = new XMLHttpRequest();
 re.open("GET", targetUrl);
 re.onload = () => {
@@ -9,7 +10,8 @@ re.onload = () => {
 re.send();
 
 let render_page = (data) => {
-  for (let i = 0; i < 9; i++) {
+  let total_stream_num = 20;
+  for (let i = 0; i < total_stream_num; i++) {
     document.querySelector(`#block-${i + 1} .display_name`).innerText =
       data.streams[i].channel.display_name;
     document.querySelector(`#block-${i + 1} .streamer-name`).innerText =
