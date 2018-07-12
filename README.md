d69078eda663b61d1b7c99304c6721bf3a7bcad3
jimmyma <jimmyma@croxlink.com>
2018-07-12 10:28:39 +0800

    add HtmlWebPackPlugin to auto make index.html

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 reference URL: [從新手到中手：前端工程加強班](https://github.com/aszx87410/frontend-intermediate-course)
 
-[Github page url](https://nicehorse06.github.io/practice-frontend-intermediate-course/index.html)
+[Github page url](https://nicehorse06.github.io/practice-frontend-intermediate-course/dist/index.html)
 
-webpack config 可以轉換ES6語法、把JS壓縮、混淆
+webpack config 可以轉換ES6語法、把JS壓縮、混淆、自動生成index.html在dist中
 
-run webpack dev server
+run webpack dev server，在記憶體中產生bundle檔案
 ```
 npm run dev
 ```
 
-產生 bundle 檔案
+產生 bundle(main.js)和index.html在 /dist 中
 ```
 npm run build
 ```
+
+/dist要讓github page讀取，未加入 gitignore
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
+      { 
+        test: /\.js$/,  // 搜索所有js檔案
+        exclude: /node_modules/,  // 除了 node_modules 檔
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader', // 使用這個 loader
           options: {
-            presets: ['env']
+            presets: ['env']  // 所有語法
           }
         }
       }
     ]
   },
+
+  // plugins 跟 loader最大的差別是 plugin 跟檔案裡的內容無關
+  plugins: [
+    //webpack會自動產生/dist/index.html，並且會有 bundle的連結
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, './index.html'),
+      filename: "./index.html"
+    })
+  ]
 }
